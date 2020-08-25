050139c199927fd35583ca51c77f4c8ae963d8de
cordoba laila <laila.merlina@gmail.com>
2020-08-24 22:04:13 -0300

    Se cambió el logo, estilo de los menúes,y traducción del menú sales.

--- src/main/webapp/app/shared/layout/header/header-components.tsx
+++ src/main/webapp/app/shared/layout/header/header-components.tsx
@@ -9,16 +9,13 @@ import appConfig from 'app/config/constants';
 
 export const BrandIcon = props => (
   <div {...props} className="brand-icon">
-    <img src="content/images/logo-jhipster.png" alt="Logo" />
+    <img src="content/images/logo-merlion.png" alt="Logo" />
   </div>
 );
 
 export const Brand = props => (
   <NavbarBrand tag={Link} to="/" className="brand-logo">
     <BrandIcon />
-    <span className="brand-title">
-      <Translate contentKey="global.title">Test</Translate>
-    </span>
     <span className="navbar-version">{appConfig.VERSION}</span>
   </NavbarBrand>
 );
